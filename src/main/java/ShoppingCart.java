851b72a6ea37d6376616fe76866e8227e35a33b2
Alexandria Paul <avpaul527@gmail.com>
2024-10-22 15:28:02 -0400

    ShoppingCart&Tax pt 1

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,2 +1,30 @@
-public class ShoppingCart {
+import java.util.ArrayList;
+
+public class ShoppingCart implements TaxOnOrders{
+    private ArrayList<Item> items = new ArrayList<>();
+
+    public void addItem(Item item, int quantity){
+        if (item.isInStock(quantity)) {
+            item.setQuantity(quantity);
+            items.add(item);
+        } else {
+            System.out.println(item.getName() + "does not have enough in stock");
+        }
+    }
+
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    public ArrayList<Item> getItems(){
+        return items;
+    }
+
+    public double
+
+
+    @Override
+    public double calculateStateTax(States state) {
+        return 0;
+    }
 }
