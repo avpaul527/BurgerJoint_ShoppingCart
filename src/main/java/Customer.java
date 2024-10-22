a3de424c0780650c2f0cf4b102ff9ff4e95c0065
Quon <k.dayquon@icloud.com>
2024-10-22 15:14:17 -0400

    Added Enum and Customer Subclass

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,13 @@
 public class Customer {
+    public String Name;
+    public String State;
+
+    public  Customer(){
+
+    }
+
+    public Customer(String name, String state) {
+        Name = name;
+        State = state;
+    }
 }
--- src/main/java/States.java
+++ src/main/java/States.java
@@ -1,2 +1,17 @@
 public enum States {
+    Delaware(0.0),
+    Pennsylvania(0.04),
+    Maryland(0.05),
+    Virginia(0.06);
+
+    private final double taxRate;
+
+    StateTax(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
 }
+
